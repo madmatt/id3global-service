96309b47044695f6cd4a329bd76715f3dfdb7ddd
Elnur Hajiyev <elnurvl@gmail.com>
2021-08-26 18:24:55 +0400

    Make `ID3BaseService` class typed

--- src/Service/ID3BaseService.php
+++ src/Service/ID3BaseService.php
@@ -7,24 +7,24 @@ abstract class ID3BaseService
      * @var bool Set to true to use the ID3 'pilot' (aka. staging or test) site for all API calls. By default, with this
      * value set to false all API calls are made against the live API.
      */
-    private $pilotSiteEnabled = false;
+    private bool $pilotSiteEnabled = false;
 
     /**
      * @var string
      */
-    private $apiUsername;
+    private string $apiUsername;
 
     /**
      * @var string
      */
-    private $apiPassword;
+    private string $apiPassword;
 
-    public function getPilotSiteEnabled()
+    public function getPilotSiteEnabled(): bool
     {
         return (bool)$this->pilotSiteEnabled;
     }
 
-    public function setPilotSiteEnabled($bool = true)
+    public function setPilotSiteEnabled($bool = true): ID3BaseService
     {
         if (!is_bool($bool)) {
             $bool = false;
@@ -34,23 +34,23 @@ abstract class ID3BaseService
         return $this;
     }
 
-    public function getApiUsername()
+    public function getApiUsername(): string
     {
         return $this->apiUsername;
     }
 
-    public function setApiUsername($username)
+    public function setApiUsername($username): ID3BaseService
     {
         $this->apiUsername = $username;
         return $this;
     }
 
-    public function getApiPassword()
+    public function getApiPassword(): string
     {
         return $this->apiPassword;
     }
 
-    public function setApiPassword($password)
+    public function setApiPassword($password): ID3BaseService
     {
         $this->apiPassword = $password;
         return $this;
